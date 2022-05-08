6ec11a4df423ff8296604c828d793eeef5666a57
adrien-intern <adrien.degrange@aodocs.com>
2022-05-08 19:24:22 +0200

    add tracks to Deezedr playlist

--- src/deezer.py
+++ src/deezer.py
@@ -1,14 +1,22 @@
-import requests
+import requests, os, yaml 
 # from requests.models import CaseInsensitiveDict
 
 def get_access_token(): 
     """ This function is not ready yet... """
 
+    PROJECT_ROOT_PATH = os.path.join(os.path.dirname(__file__), '..')
+    with open(os.path.join(PROJECT_ROOT_PATH, 'data', 'config.yaml'), 'r') as config_file: 
+      config = yaml.load(config_file, Loader=yaml.FullLoader)
+    deezer = config['credentials']['deezer'] 
+
     # See https://developers.deezer.com/api/oauth & https://developers.deezer.com/myapps (app's page)
-    query_params = { 'app_id': '$APP_ID', 'secret': '$APP_SECRET', 'redirect_uri': '$REDIRECT_URI', 'perms': 'basic_access,email,offline_access' }
+    # "manage_library" scope is required to manage the playlists
+    # Note: when a NEW scope is required, add on at the TOP of the list
+    query_params = { 'app_id': deezer['app_id'], 'secret': deezer['app_secret'], 'redirect_uri': deezer['app_redirect_uri'], \
+        'perms': 'basic_access,email,manage_library,offline_access' }
     # "offline_access" is needed 
-    # response = requests.get('https://connect.deezer.com/oauth/auth.php', params=query_params)
-    # print(response.json())
+    response = requests.get('https://connect.deezer.com/oauth/auth.php', params=query_params)
+    print(response.request.url)
     # => Code: abcd... 
 
     query_params = { 'app_id': '$APP_ID', 'secret': '$APP_SECRET', 'code': '$CODE' } 
@@ -16,6 +24,10 @@ def get_access_token():
     # => Access token: xxx-xxx-xxx-xxx 
     # response = requests.get('https://connect.deezer.com/oauth/access_token.php', params=query_params) 
     # print(response.json())
+
+
+# if __name__ == "__main__":
+#     get_access_token()
 
 
 def search_track(access_token, track_name, artist_names='', temp_out_file=''): 
@@ -43,5 +55,7 @@ def search_track(access_token, track_name, artist_names='', temp_out_file=''):
 
     return response_json['total'], (response_json['data'][0]['id'] if response_json['data'] else None)
 
-
-
+def add_track_to_playlist(access_token, playlist_id, track_id):
+    query_params = { 'access_token': access_token }
+    body_params = { 'songs': [track_id] }
+    requests.post(f'https://api.deezer.com/playlist/{playlist_id}/tracks', params=query_params, data=body_params)
