f46e669f476a70f490e4eac53b5b72766f571879
adrien-intern <adrien.degrange@aodocs.com>
2022-11-05 14:02:08 +0100

    improved check_key_and_return_value: now accepts tuples, and IDs in case the object is a list or tuple

--- src/FileHelpers.py
+++ src/FileHelpers.py
@@ -2,23 +2,44 @@ import os
 from typing import Any 
 import yaml 
 
-def check_key_and_return_value(object: Any, key_name: str | list)->Any:
-    """Test whether `key_name` is contained in `object`
+def check_key_and_return_value(object: Any, key: str | list)->Any:
+    """TL; DR; Test whether `key` can be found in `object`
     If not, throws an exception. 
-    `key_name` wan be a list, e.g. ['a', 'b', 'c']
-    In that case, the method looks for object['a']['b']['c']"""
 
-    if isinstance(key_name, list):
-        keys = key_name
-    else: 
-        keys = [key_name]
+    `key` can be a list, e.g. ['a', 'b', 'c']
+    In that case, the method looks for object['a']['b']['c']
+    
+    Details: If `key` is of type str, then `object` is assumed to be a dict
+        If so, `object`[`key`] is returned 
+
+    If `key` if of type int, the type of `object` is checked: 
+        - If dict, behavior is unchanged
+        - If list or tuple, we check whether the length of `object` > `key`
+            (if so, the `key`th element is returned) 
+    """
+
+    if isinstance(key, list):
+        keys = key
+    elif isinstance(key, tuple):
+        keys = list(key) 
+    else: # a 1-level key
+        keys = [key]
     
     current_object = object
 
     for k in keys: 
-        if not k in current_object:
-            raise KeyError(f"The key {'>'.join(keys)} (specifically {k}) cannot be found in the object")
-        
+        match k: 
+            case str() | float():
+                if not k in current_object:
+                    raise KeyError(f"The key {'>'.join(keys)} (specifically { k }) cannot be found in the object")
+            case int():
+                if (isinstance(current_object, list) or isinstance(current_object, tuple)) and (k >= len(current_object)): 
+                    raise KeyError(f"The {'>'.join(keys)}th element cannot be retrieved from the object, \
+                        since the list/tuple only contains { len(current_object) } element(s)") 
+            case _:
+                raise KeyError(f'The (sub-)key { k } is of type { type(k) }, \
+                    which is not supported')
+
         current_object = current_object[k]
 
     return current_object
